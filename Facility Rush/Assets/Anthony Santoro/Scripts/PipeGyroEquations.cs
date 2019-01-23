6b811fae661b0746f020dcfb3f39b6f8b716d6e3
Crucidian <Crucidian@users.noreply.github.com>
2019-01-23 14:23:00 -0500

    Adding in the Minigame Select and the Options Menu

---
+++ Facility Rush/Assets/Anthony Santoro/Scripts/PipeGyroEquations.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeGyroEquations : MonoBehaviour
+{
+    public bool gradeK, grade1, grade2, grade3, grade4, grade5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
