8847b14a586893dad030e110012508ec92a86f63
Mitchell_Ford <MJFord64@outlook.com>
2019-01-24 13:54:36 -0500

    Main Menu Functionality
    Hooking up UI for the first build with the rest of the teams project.

---
+++ Facility Rush/Assets/Mitchell/Scripts/MF_MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MF_MainMenu : MonoBehaviour
+{
+    public static string PreviousScene = "";
+
+    void start()
+    {
+        PreviousScene = SceneManager.GetActiveScene().name;
+    }
+
+    void OnClickBegin()
+    {
+        PreviousScene = SceneManager.GetActiveScene().name;
+    }
+
+    public void OnClickOptions()
+    {
+        PreviousScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene("Options Menu"); 
+    }
+
+    public void OnClickExitOptions()
+    {
+        SceneManager.LoadScene(PreviousScene);
+    }
+}
---
+++ Facility Rush/Assets/Mitchell/Scripts/MF_Pause.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MF_Pause : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnClickPause()
+    {
+
+    }
+}
