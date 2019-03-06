e2cd7a93009d232e36e0d281541c48cbc9b61db3
williamfs <CSheu@student.fullsail.edu>
2019-03-06 10:44:20 -0500

    Tutorial Scripts

---
+++ Facility Rush/Assets/William/Scripts/FamiliarSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FamiliarSystem : MonoBehaviour
+{
+    public delegate void DialogContainer(int i);  //declare the method signiture of type delegate
+    public static DialogContainer PopDialog;      //declare the member variable(container) of type delegate that we just made
+
+    public GameObject[] placeHolders;
+    public static FamiliarSystem instance;
+
+    private void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+}
---
+++ Facility Rush/Assets/William/Scripts/Tutorial.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+    public string[] dialog;
+    public string[] Conditions;
+
+    private Text text;
+
+    private void Start()
+    {
+        FamiliarSystem.PopDialog(0);
+
+    }
+    void Update()
+    {
+        Next();
+    }
+    private void OnEnable()
+    {
+        text = GetComponent<Text>();
+        FamiliarSystem.PopDialog += ChangeText;
+    }
+
+    private void OnDisable()
+    {
+        FamiliarSystem.PopDialog -= ChangeText;
+    }
+
+    void ChangeText(int index)
+    {
+        text.text = dialog[index];
+    }
+
+    void Next()
+    {
+        if (Input.touches.Length != 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            this.gameObject.SetActive(false);
+        }*/
+    }
+}
