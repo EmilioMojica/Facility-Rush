8bf6acec2a7a9cad2534c6e07592435321b107a4
williamfs <CSheu@student.fullsail.edu>
2019-03-07 14:44:50 -0500

    Music implementation started
    Imported Dern's music and SF, started to implement AudioManager.

---
+++ Facility Rush/Assets/William/Scripts/AudioManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    public AudioSource audioSource;
+    public AudioClip[] musicClip;
+    public AudioClip[] soundClip;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = musicClip[1];
+        audioSource.Play();
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ Facility Rush/Assets/William/Scripts/CatchAudio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeMusic(AudioClip clip)
+    {
+
+        AudioManager.instance.audioSource.clip = clip;
+        AudioManager.instance.audioSource.Play();
+        Debug.Log("Uses static way");
+    }
+}
