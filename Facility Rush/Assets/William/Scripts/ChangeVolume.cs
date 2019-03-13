81ed59587d245bed26ab1c09882f77ea1bc7735a
williamfs <CSheu@student.fullsail.edu>
2019-03-13 17:04:31 -0400

    Audio cont

--- Facility Rush/Assets/William/Scripts/ChangeVolume.cs
+++ Facility Rush/Assets/William/Scripts/ChangeVolume.cs
@@ -11,9 +11,6 @@ public class ChangeVolume : MonoBehaviour
     [SerializeField] private Slider soundSlider;
     [SerializeField] private float soundValue;  // stores the volume value which it's corresponding audio source has
 
-    [SerializeField] private AudioSource audioSource;
-
-
     private void Awake()
     {
         musicSlider = GameObject.FindObjectOfType<SliderHolder>().sliders[0];  //取得slider的控制權
@@ -24,17 +21,14 @@ public class ChangeVolume : MonoBehaviour
     void Start()  //目標: 讓每個場景中的slider根據PlayerPrefs的值去自動調整
     {
         musicSlider.value = PlayerPrefs.GetFloat("music");
-        //TODO: soundSlider 
-        //AudioManager.instance.musicAudioSource.volume = musicSlider.value;  //根據slider的位置來更改真正的audio source聲音
-        //TODO: soundSlider 
-
+        soundSlider.value = PlayerPrefs.GetFloat("sound");
     }
 
     public void ChangeMusicVolume()   // Called in the inspector of slider built-in OnValueChange
     {
         AudioManager.instance.musicAudioSource.volume = musicSlider.value;  //改變slider的音量大小會真正影響到audio source的音量大小
         musicValue = AudioManager.instance.musicAudioSource.volume;         
-        PlayerPrefs.SetFloat("music", musicValue);                          //並把這個改變的值存進Player"Prefs以供日後讀檔需要
+        PlayerPrefs.SetFloat("music", musicValue);                          //並把這個改變的值存進PlayerPrefs以供日後讀檔需要
     }
 
     public void ChangeSoundVolume()
