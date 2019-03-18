4b1a50f0efb00ed4595f7d17c56ddc64fe6be2d7
Jon <premiumjon13@gmail.com>
2019-03-18 15:18:57 -0400

    Update ChutesDrag.cs
    Earned my credits on Facility Rush

--- Facility Rush/Assets/Lin/Script/ChutesDrag.cs
+++ Facility Rush/Assets/Lin/Script/ChutesDrag.cs
@@ -14,6 +14,8 @@ public class ChutesDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
 
     public static GameObject itemBeingDragged;
 
+    private Vector3 initial;
+
     public void Awake()
     {
         positionone = transform.position;
@@ -21,37 +23,42 @@ public class ChutesDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        initial = this.transform.position;
         itemBeingDragged = gameObject;
         backPosition = transform.position;
         startPosition = transform.position;
         GetComponent<CanvasGroup>().blocksRaycasts = false;
 
-        AudioManager.instance.soundAudioSource.clip = AudioManager.instance.soundClip[0];
-        AudioManager.instance.soundAudioSource.Play();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = Input.mousePosition;
+
+        Vector3 currentPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, initial.z);
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(currentPosition);
+        this.transform.position = worldPosition;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+
         itemBeingDragged = null;
      
         transform.position = startPosition;
         GetComponent<CanvasGroup>().blocksRaycasts = true;
 
-        AudioManager.instance.soundAudioSource.clip = AudioManager.instance.soundClip[1];
-        AudioManager.instance.soundAudioSource.Play();
     }
 
 
     public void MoveBack()
     {
+      
         transform.SetParent(startParent);
         transform.position = positionone;
         GetComponent<CanvasGroup>().blocksRaycasts = true;
+        
     }
+
+  
 }
 
