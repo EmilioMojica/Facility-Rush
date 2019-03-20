5e5df0341a5ea00b648ef3372a593966828f4cb1
Brennan Kersey <bksafari17@gmail.com>
2019-03-20 09:50:05 -0400

    Updates to Drag Handler plus tutorial scenes

--- Facility Rush/Assets/Brennan Kersey/Scripts/DragHandler.cs
+++ Facility Rush/Assets/Brennan Kersey/Scripts/DragHandler.cs
@@ -5,17 +5,27 @@ using UnityEngine.EventSystems;
 
 public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    private Vector3 startPosition;
+    public Vector3 startPosition;
+    public Vector3 backPosition;
+    public Vector3 positionone;
 
     public static GameObject itemBeingDragged;
 
     private Transform startParent;
-   
+
+    private Vector3 initial;
+
+    public void Awake()
+    {
+        positionone = transform.position;
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
         //throw new System.NotImplementedException();
+        initial = this.transform.position;
         itemBeingDragged = gameObject;
         startPosition = transform.position;
+        backPosition = transform.position;
         startParent = transform.parent;
         GetComponent<CanvasGroup>().blocksRaycasts = false;
 
@@ -25,7 +35,11 @@ public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = Input.mousePosition;
+        // transform.position = Input.mousePosition;
+
+        Vector3 currentPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, initial.z);
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(currentPosition);
+        this.transform.position = worldPosition;    //worldPosition
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -36,5 +50,14 @@ public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 
         AudioManager.instance.soundAudioSource.clip = AudioManager.instance.soundClip[1];
         AudioManager.instance.soundAudioSource.Play();
+    }
+
+    public void MoveBack()
+    {
+
+        transform.SetParent(startParent);
+        transform.position = positionone;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
     }
 }
