9714ef7391215876f3c6f8ac07c16b37940a1c5a
williamfs <CSheu@student.fullsail.edu>
2019-03-21 14:15:55 -0400

    Chutes scene Audio Implemented
    Pick up/ drop box SFX implemented & Pause Menu Music/SFX slider connected with Audio Manager

--- Facility Rush/Assets/William/Scripts/SliderHolder.cs
+++ Facility Rush/Assets/William/Scripts/SliderHolder.cs
@@ -3,20 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-/*Script goal: holds music and sound slider in every scenes so that each time Change Value script can get hold of these sliders even if those sliders are unenabled*/
+/*Script goal: holds music and sound slider in every scenes so that each time Change Volume script can get hold of these sliders even if those sliders are unenabled*/
 public class SliderHolder : MonoBehaviour
 {
     public Slider[] sliders;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
