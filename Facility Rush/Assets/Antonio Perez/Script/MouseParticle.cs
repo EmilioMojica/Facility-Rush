e3851a6970bd8c4a99311d2c336ace6764c064a5
aperez440 <aperez440@yahoo.com>
2019-03-22 10:26:07 -0400

    Commit
    Adding changes to the particles

---
+++ Facility Rush/Assets/Antonio Perez/Script/MouseParticle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseParticle : MonoBehaviour
+{
+    private int distance = 8;
+
+    private void Start()
+    {
+        //GetComponent<ParticleSystem>().Stop();
+    }
+
+    private void Update()
+    {
+        if (!Input.GetMouseButtonDown(0))
+        {
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            transform.position = r.GetPoint(distance);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<ParticleSystem>().Play();
+        }
+    }
+}
