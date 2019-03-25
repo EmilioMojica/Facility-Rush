adef671ac624ad648bfaf55cc9f564e22de3caa1
Lin Qi <>
2019-03-25 10:33:39 -0400

    Some anchor changes and time stop particle fix

--- Facility Rush/Assets/Antonio Perez/Script/DontDestroy.cs
+++ Facility Rush/Assets/Antonio Perez/Script/DontDestroy.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
+    public static DontDestroy Instance;
     private void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
         DontDestroyOnLoad(this.gameObject);
     }
 }
