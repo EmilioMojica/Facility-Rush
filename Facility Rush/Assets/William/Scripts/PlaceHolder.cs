d707e7ee07445965db405b9db84b27114117a193
williamfs <CSheu@student.fullsail.edu>
2019-04-02 16:35:21 -0400

    MainMenu bug fixed
    After you enter any game and return to MainMenu, you can't go into any other game again. This bug is fixed.

---
+++ Facility Rush/Assets/William/Scripts/PlaceHolder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceHolder : MonoBehaviour
+{
+    public DegradationManager degradationManager;
+    public LevelChanger levelChanger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        degradationManager = GameObject.FindObjectOfType<DegradationManager>();
+        levelChanger = GameObject.FindObjectOfType<LevelChanger>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CallsetAllBoolSToFalse()
+    {
+        degradationManager.setAllBoolSToFalse();
+    }
+
+    public void CallFadeToLevel(int levelIndex)
+    {
+        levelChanger.FadeToLevel(levelIndex);
+    }
+}
